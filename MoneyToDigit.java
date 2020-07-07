2032c7f5162ace67123a62876d6ca462a24a167f
qiufujian <1663072318@qq.com>
2020-07-07 22:30:50 +0800

    修改为中文编码

--- MoneyToDigit.java
+++ MoneyToDigit.java
@@ -6,89 +6,89 @@ import java.util.Map;
 public class MoneyToDigit {
 	private static Map<Character, Integer> map = new HashMap<Character, Integer>();
 	static {
-		map.put('Áã', 0);
-		map.put('Ò¼', 1);
-		map.put('·¡', 2);
-		map.put('Èþ', 3);
-		map.put('ËÁ', 4);
-		map.put('Îé', 5);
-		map.put('Â½', 6);
-		map.put('Æâ', 7);
-		map.put('°Æ', 8);
-		map.put('¾Á', 9);
-		map.put('Ê°', 10);
-		map.put('°Û', 100);
-		map.put('Çª', 1000);
-		map.put('Íò', 10000);
-	 // map.put('ÒÚ', 100000000);
+		map.put('é›¶', 0);
+		map.put('å£¹', 1);
+		map.put('è´°', 2);
+		map.put('å', 3);
+		map.put('è‚†', 4);
+		map.put('ä¼', 5);
+		map.put('é™†', 6);
+		map.put('æŸ’', 7);
+		map.put('æŒ', 8);
+		map.put('çŽ–', 9);
+		map.put('æ‹¾', 10);
+		map.put('ä½°', 100);
+		map.put('ä»Ÿ', 1000);
+		map.put('ä¸‡', 10000);
+	    map.put('äº¿', 100000000);
 	}
 	
 	
 	public static void main(String args[]) {
-		String test1 = "Ò¼ÒÚ·¡ÇªÈþ°ÛËÁÊ°Ò¼ÍòÁã·¡Ê°Ò¼Ôª";
+		String test1 = "å£¹äº¿è´°ä»Ÿåä½°è‚†æ‹¾å£¹ä¸‡é›¶è´°æ‹¾å£¹å…ƒ";
 		System.out.println(MoneyStrToNum(test1));
 		// 123410021
 		
-		String test2 = "Èþ°Û°ÆÊ°Ôª";
+		String test2 = "åä½°æŒæ‹¾å…ƒ";
 		System.out.println(MoneyStrToNum(test2));
 		// 380
 		
-		String test3 = "·¡ÍòËÁÇªÁã°ÆÒÚ·¡ÇªËÁ°ÛÁãÒ¼ÍòÁã·¡Ê°Ò¼Ôª";
+		String test3 = "è´°ä¸‡è‚†ä»Ÿé›¶æŒäº¿è´°ä»Ÿè‚†ä½°é›¶å£¹ä¸‡é›¶è´°æ‹¾å£¹å…ƒ";
 		System.out.println(MoneyStrToNum(test3));
 		// 2400824010021
 		
-		String test4 = "Ò¼Ê°ÔªÕû";
+		String test4 = "å£¹æ‹¾å…ƒæ•´";
 		System.out.println(MoneyStrToNum(test4));
 		// 10
 	}
 	
-	// Ê°ÍòÔªÕû
+	// æ‹¾ä¸‡å…ƒæ•´
 	public static long MoneyStrToNum(String s) {
 		if(s == null || s.length() == 0)
 			return 0;
-		if(s.indexOf('ÒÚ') > 0) {
-			String[] strs = s.split("ÒÚ");
+		if(s.indexOf('äº¿') > 0) {
+			String[] strs = s.split("äº¿");
 			if(strs.length < 2)
 				return MoneyStrToNum(strs[0]) * 100000000;
 			else
 				return MoneyStrToNum(strs[0]) * 100000000 +  MoneyStrToNum(strs[1]);
 		}
-		else if(s.indexOf('Íò') > 0){
-			String[] strs = s.split("Íò");
+		else if(s.indexOf('ä¸‡') > 0){
+			String[] strs = s.split("ä¸‡");
 			if(strs.length < 2)
 				return MoneyStrToNum(strs[0]) * 10000;
 			else
 				return MoneyStrToNum(strs[0]) * 10000 +  MoneyStrToNum(strs[1]);
 		}
-		else if(s.indexOf('Çª') > 0) {
-			String[] strs = s.split("Çª");
+		else if(s.indexOf('ä»Ÿ') > 0) {
+			String[] strs = s.split("ä»Ÿ");
 			if(strs.length < 2)
 				return MoneyStrToNum(strs[0]) * 1000;
 			else
 				return MoneyStrToNum(strs[0]) * 1000 +  MoneyStrToNum(strs[1]);
 		}
-		else if(s.indexOf('°Û') > 0 ) {
-			String[] strs = s.split("°Û");
+		else if(s.indexOf('ä½°') > 0 ) {
+			String[] strs = s.split("ä½°");
 			if(strs.length < 2)
 				return MoneyStrToNum(strs[0]) * 100;
 			else
 				return MoneyStrToNum(strs[0]) * 100 +  MoneyStrToNum(strs[1]);
 		}
-		else if(s.indexOf('Ê°') > 0 ) {
-			String[] strs = s.split("Ê°");
+		else if(s.indexOf('æ‹¾') > 0 ) {
+			String[] strs = s.split("æ‹¾");
 			if(strs.length < 2)
 				return MoneyStrToNum(strs[0]) * 10;
 			else
 				return MoneyStrToNum(strs[0]) * 10 +  MoneyStrToNum(strs[1]);
 		}
-		else if(s.indexOf('Ôª') > 0 ) {
-			int index = s.indexOf('Ôª');
+		else if(s.indexOf('å…ƒ') > 0 ) {
+			int index = s.indexOf('å…ƒ');
 			if(index == 0)
 				return 0;
 			int result = map.getOrDefault(s.charAt(index - 1), 0);
 			return result;
 		}
-		else if(s.indexOf('Áã') >= 0 ) {
+		else if(s.indexOf('é›¶') >= 0 ) {
 				return MoneyStrToNum(s.substring(1));
 		}
 		else {
